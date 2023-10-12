240aeba8b2fd64f18b8a2e5538e1030bf9f49c5f
nderim-strong <nderim@strong-network.net>
2023-10-12 14:42:08 +0200

    add user model

---
+++ ScsMarketplace.API/Models/User/UserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ScsMarketplace.API.Models.Account
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public int UsertypeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+        public int LenderId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public string SessionId { get; set; }
+    }
+}
